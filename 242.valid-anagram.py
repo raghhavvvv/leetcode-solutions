8ac7959fffac6a46db58486fbb4bd06a37afb13f
raghhavvvv <raghavpant443@gmail.com>
2025-06-25 13:39:21 +0530

    More solutions

---
+++ 242.valid-anagram.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=242 lang=python3
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+        
+        
+# @lc code=end
+
