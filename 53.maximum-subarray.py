fa1eaea593c64fe0a9a3693e4c725181db8fca62
raghhavvvv <raghavpant443@gmail.com>
2025-06-25 15:14:55 +0530

    one more solution

---
+++ 53.maximum-subarray.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=53 lang=python3
+#
+# [53] Maximum Subarray
+#
+
+# @lc code=start
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+        return maxSub
+
+        
+# @lc code=end
+
